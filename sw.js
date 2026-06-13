8d88e59883e85abaa450f0f9aa62d47e2aeab7b8
franktembo23-ai <franktembo23@gmail.com>
2026-06-13 14:47:23 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,2 @@
+self.addEventListener('install', e => { e.waitUntil(caches.open('mtc-v3').then(c => c.addAll(['./','./index.html','./manifest.json','./icon-512.png']))); });
+self.addEventListener('fetch', e => { e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))); });
